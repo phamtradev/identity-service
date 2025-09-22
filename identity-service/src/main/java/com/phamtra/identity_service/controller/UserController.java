b04c22334e7408129f6dda406c994ed0d82cf8b8
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-09-22 18:58:40 +0700

    feat: config ResponeEntity for api endpoints

--- identity-service/src/main/java/com/phamtra/identity_service/controller/UserController.java
+++ identity-service/src/main/java/com/phamtra/identity_service/controller/UserController.java
@@ -3,6 +3,7 @@ package com.phamtra.identity_service.controller;
 import com.phamtra.identity_service.dto.request.UserDTORequest;
 import com.phamtra.identity_service.entity.User;
 import com.phamtra.identity_service.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,28 +19,31 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody UserDTORequest request) {
-        return this.userService.createUser(request);
+    public ResponseEntity<?> createUser(@RequestBody UserDTORequest request) {
+        User user = this.userService.createUser(request);
+        return ResponseEntity.ok().body(user);
     }
 
     @GetMapping("/users")
-    public List<User> getUsers() {
-        return this.userService.getUsers();
+    public ResponseEntity<?> getUsers() {
+        return ResponseEntity.ok().body(this.userService.getUsers());
     }
 
     @GetMapping("/users/{id}")
-    public User getUserById(@PathVariable("id") long id) {
-        return this.userService.getUserbyId(id);
+    public ResponseEntity<?> getUserById(@PathVariable("id") long id) {
+        User user = this.userService.getUserbyId(id);
+        return ResponseEntity.ok().body(user);
     }
 
     @PutMapping("/users/{id}")
-    public User updateUser(@PathVariable("id") long id, @RequestBody UserDTORequest request) {
-        return this.userService.updateUser(id, request);
+    public ResponseEntity<?> updateUser(@PathVariable("id") long id, @RequestBody UserDTORequest request) {
+        User user = this.userService.updateUser(id, request);
+        return ResponseEntity.ok().body(user);
     }
 
     @DeleteMapping("/users/{id}")
-    public String deleteUser(@PathVariable("id") long id) {
+    public ResponseEntity<?> deleteUser(@PathVariable("id") long id) {
         this.userService.deleteUser(id);
-        return "success!";
+        return ResponseEntity.ok().body("successfuly");
     }
 }
