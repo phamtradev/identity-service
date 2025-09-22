b1dd41ae1376aaee53467b17ea054aea1b1c89b0
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-09-22 19:20:46 +0700

    feat: format respone before sending

--- identity-service/src/main/java/com/phamtra/identity_service/exception/GlobalException.java
+++ identity-service/src/main/java/com/phamtra/identity_service/exception/GlobalException.java
@@ -1,5 +1,7 @@
 package com.phamtra.identity_service.exception;
 
+import com.phamtra.identity_service.dto.respone.RestRespone;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,7 +9,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalException {
     @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<String> handlIdException(IdInvalidException idInvalidException) {
-        return ResponseEntity.badRequest().body(idInvalidException.getMessage());
+    public ResponseEntity<RestRespone<Object>> handlIdException(IdInvalidException idInvalidException) {
+        RestRespone<Object> res = new RestRespone<>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setError(idInvalidException.getMessage());
+        res.setMessage("IdInvalidException");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
 }
---
+++ identity-service/src/main/java/com/phamtra/identity_service/util/FormatRespone.java
@@ -0,0 +1,41 @@
+package com.phamtra.identity_service.util;
+
+import com.phamtra.identity_service.dto.respone.RestRespone;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class FormatRespone implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
+        HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        int status = servletResponse.getStatus();
+        RestRespone<Object> res = new RestRespone<>();
+        res.setStatusCode(status);
+        if (status >= 400) {
+            return body;
+        } else {
+            res.setData("CALL API SUCCESS");
+        }
+        return res;
+    }
+}
