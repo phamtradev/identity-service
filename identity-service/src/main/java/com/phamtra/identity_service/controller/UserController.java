c11704380785004dfdca9d9398714224dac8127e
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-09-22 18:41:41 +0700

    feat: crud user

--- identity-service/src/main/java/com/phamtra/identity_service/controller/UserController.java
+++ identity-service/src/main/java/com/phamtra/identity_service/controller/UserController.java
@@ -3,10 +3,9 @@ package com.phamtra.identity_service.controller;
 import com.phamtra.identity_service.dto.request.UserDTORequest;
 import com.phamtra.identity_service.entity.User;
 import com.phamtra.identity_service.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
@@ -21,5 +20,26 @@ public class UserController {
     @PostMapping("/users")
     public User createUser(@RequestBody UserDTORequest request) {
         return this.userService.createUser(request);
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return this.userService.getUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable("id") long id) {
+        return this.userService.getUserbyId(id);
+    }
+
+    @PutMapping("/users/{id}")
+    public User updateUser(@PathVariable("id") long id, @RequestBody UserDTORequest request) {
+        return this.userService.updateUser(id, request);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.deleteUser(id);
+        return "success!";
     }
 }
--- identity-service/src/main/java/com/phamtra/identity_service/service/UserService.java
+++ identity-service/src/main/java/com/phamtra/identity_service/service/UserService.java
@@ -5,6 +5,9 @@ import com.phamtra.identity_service.entity.User;
 import com.phamtra.identity_service.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -24,5 +27,33 @@ public class UserService {
         user.setDob(request.getDob());
 
         return this.userRepository.save(user);
+    }
+
+    public User updateUser(long id, UserDTORequest request) {
+        User user = getUserbyId(id);
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstname(request.getFirstname());
+        user.setLastname(request.getLastname());
+        user.setDob(request.getDob());
+
+        return this.userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public User getUserbyId(long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        return null;
+    }
+
+    public void deleteUser(long id) {
+        this.userRepository.deleteById(id);
     }
 }
