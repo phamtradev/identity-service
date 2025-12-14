da5334d212f88acd86ac90eb16bbfac15f43e5da
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-12-14 18:40:11 +0700

    feat: config Dockerfile

--- identity-service/src/main/java/com/phamtra/identity_service/dto/request/LoginDTO.java
+++ identity-service/src/main/java/com/phamtra/identity_service/dto/request/LoginDTO.java
@@ -1,10 +1,10 @@
 package com.phamtra.identity_service.dto.request;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 public class LoginDTO {
 
     private String username;
--- identity-service/src/main/java/com/phamtra/identity_service/dto/respone/LoginDTORespone.java
+++ identity-service/src/main/java/com/phamtra/identity_service/dto/respone/LoginDTORespone.java
@@ -1,10 +1,10 @@
 package com.phamtra.identity_service.dto.respone;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 public class LoginDTORespone {
     private String accessToken;
 }
